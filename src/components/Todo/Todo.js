bed84228676e05d7238ffea1802cf339807e4164
Suketu.P <suketu.p@simformsolutions.com>
2020-10-05 00:54:36 +0530

    add style and edit funcitonality

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
+import "./Todo.css"
 
 function Todo() {
   const [todos, setTodos] = useState([]);
   const [todoText, setTodoText] = useState("");
+  const [editValue, setEditValue] = useState("");
+  const [editingMode, setEditingMode] = useState(false);
   const addTodo = () => {
+    if (!todoText.trim()) {
+      return;
+    }
     const todoObj = {
       id: uuid(),
       text: todoText,
       isCompleted: false,
+      isEditing: false
     };
     setTodos([...todos, todoObj]);
+    setTodoText("");
   };
   const deleteTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
+
+  const setEditMode = (todo, bool) => {
+    if (bool) {
+      setEditingMode(true)
+      setEditValue(todo.text)
+    }
+    else {
+      setEditingMode(false)
+    }
+    const newTodos = todos.map(item => {
+      if (item.id === todo.id) {
+        item.isEditing = bool
+        item.text = editValue
+      }
+      return item;
+    })
+    setTodos(newTodos)
+  }
+
   const markComplete = (id) => {
     setTodos(
       todos.map((todo) => {
@@ -25,7 +52,6 @@ function Todo() {
       })
     );
   };
-  console.log("todos", todos);
   return (
     <div>
       <div
@@ -38,47 +64,67 @@ function Todo() {
         <div>
           <input
             type="text"
-            style={{ width: "50vw" }}
+            className="input"
+            value={todoText}
+            placeholder="Press 'Enter' or Click 'Add' to add todo"
             onChange={(e) => setTodoText(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                addTodo()
+              }
+            }}
           ></input>
         </div>
-        <div style={{ margin: "0px 10px", cursor: 'pointer'}}>
-          <input type="button" value="ADD TODO" onClick={addTodo}></input>
+        <div style={{ margin: "0px 10px", cursor: 'pointer' }}>
+          <input className="inputBtn btn" type="button" value="ADD" onClick={addTodo}></input>
         </div>
       </div>
       {todos.map((todo, index) => {
         return (
           <div
             key={todo.id}
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              border: "2px dashed black",
-              padding: "2vh 3vw",
-              margin: "2vh 0vw",
-              backgroundColor: todo.isCompleted ? "green" : "red",
-            }}
+            className={"todo " + (!todo.isCompleted ? "todoRed" : "todoGreen")}
           >
-            <div>{index}</div>
-            <div>{todo.text}</div>
+            <div className="index">{index + 1}</div>
+            {todo.isEditing ?
+              <input
+                type="text"
+                className="editBox"
+                value={editValue}
+                onChange={(e) => setEditValue(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    setEditMode(todo, false)
+                  }
+                }}
+                autoFocus
+              />
+              :
+              <div onDoubleClick={() => {
+                !editingMode && setEditMode(todo, true)
+              }}>{todo.text}</div>
+            }
             <div style={{ display: "flex" }}>
-              <div>
-                <input
-                  type="button"
-                  value="Delete"
-                  onClick={() => deleteTodo(todo.id)}
-                ></input>
-              </div>
               <div
                 style={{
                   display: todo.isCompleted ? "none" : "block",
-                  margin: "0px 0px 0px 3px",
                 }}
               >
                 <input
+                  className={"btn " + (editingMode ? "disabled" : "complete")}
                   type="button"
-                  value="Mark Complete"
+                  value="Done"
                   onClick={() => markComplete(todo.id)}
+                  disabled={editingMode}
+                ></input>
+              </div>
+              <div>
+                <input
+                  className={"btn " + (editingMode ? "disabled" : "delete")}
+                  type="button"
+                  value="Delete"
+                  onClick={() => deleteTodo(todo.id)}
+                  disabled={editingMode}
                 ></input>
               </div>
             </div>
